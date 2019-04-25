50ed9cf41ea4fe2ec737c5aee9398b0a74256cd8
evahfr <evahfr@gmail.com>
2019-04-25 22:03:48 +0200

    Oppdatert kart for Henningsvær og Bodø

--- Prosjekt/script/map.js
+++ Prosjekt/script/map.js
@@ -1,3 +1,4 @@
+// ----Kartdataene------
 var lofotenMapData = [{
     "type": "FeatureCollection",
     "features": [
@@ -250,11 +251,448 @@ var lofotenMapData = [{
     ]
   }];
 
-document.body.onload = function() {
-    var map = L.map('map-lofoten', {
-        scrollWheelZoom: false
-    }).setView([68.175040, 14.084761], 8);
+var henningsvaerMapData = [{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+            "popupContent": "Start og avsluttning på reise. Dag 1: resisen starter fra Henningsvær. Dag 2: reisen avslutter på Henningsvær."
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            14.201459884643553,
+            68.15076956033836
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "popupContent": "Dag 1: Nydelig fiskemiddag med dagens fangst. Mulighet for hvalsafari fra Å."
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            12.987899780273438,
+            67.87974328689724
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "popupContent": "Dag 2: Stop på Værøy med mulighet for avslapping, fottur eller kajakkpaddling."
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            12.63702392578125,
+            67.65416574898566
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              14.201459884643553,
+              68.15076956033836
+            ],
+            [
+              12.987942695617676,
+              67.8797594467422
+            ],
+            [
+              12.986526489257812,
+              67.85096098306639
+            ],
+            [
+              12.903442382812498,
+              67.73339440055
+            ],
+            [
+              12.7935791015625,
+              67.64581034758912
+            ],
+            [
+              12.6947021484375,
+              67.62804527718282
+            ],
+            [
+              12.661056518554688,
+              67.63301039473585
+            ],
+            [
+              12.637710571289062,
+              67.65364362321732
+            ],
+            [
+              12.64251708984375,
+              67.61445118724366
+            ],
+            [
+              12.655563354492188,
+              67.60346562545749
+            ],
+            [
+              12.723541259765625,
+              67.5956156649846
+            ],
+            [
+              12.805938720703123,
+              67.60372724587064
+            ],
+            [
+              12.919921874999998,
+              67.62909065197242
+            ],
+            [
+              14.20154571533203,
+              68.15057790022045
+            ]
+          ]
+        }
+      }
+    ]
+  }];
 
+var bodoeMapData = [{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "popupContent": "Start og avsluttning på reise. Dag 1: Avreise fra Bodø. Dag 7: Avslutter reise i Bodø."
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            14.364151954650877,
+            67.28379544571632
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "popupContent": "Dag 1: Saltstrømmen, med mulighet for ribb."
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            14.61078643798828,
+            67.23181761428154
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "popupContent": "Dag 2: Stopp på Fugløya, med muligheter for fisketur eller gåtur."
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            13.772735595703123,
+            67.05137505309885
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "popupContent": "Dag 3: Stopp i Glomfjord, med mulighet for bretur på Svartisen."
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            13.706130981445312,
+            66.70245313735215
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "popupContent": "Dag 4: Svartisen fra avstand."
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            13.977355957031248,
+            66.80783515142156
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "popupContent": "Dag 5-6: Avslapping på båten."
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            13.415679931640625,
+            67.13903070858395
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              14.364280700683594,
+              67.28377887322337
+            ],
+            [
+              14.313983917236328,
+              67.27064986259232
+            ],
+            [
+              14.302654266357422,
+              67.26394997370087
+            ],
+            [
+              14.299392700195312,
+              67.25452716489158
+            ],
+            [
+              14.309005737304688,
+              67.24788916513515
+            ],
+            [
+              14.325828552246094,
+              67.24629577226821
+            ],
+            [
+              14.374923706054686,
+              67.2472252642809
+            ],
+            [
+              14.467620849609375,
+              67.24788916513515
+            ],
+            [
+              14.558258056640623,
+              67.24788916513515
+            ],
+            [
+              14.607696533203125,
+              67.2415149596819
+            ],
+            [
+              14.611473083496096,
+              67.23248194080642
+            ],
+            [
+              14.602546691894531,
+              67.23925702313805
+            ],
+            [
+              14.575080871582031,
+              67.2415149596819
+            ],
+            [
+              14.478950500488283,
+              67.24284305857493
+            ],
+            [
+              14.35020446777344,
+              67.22610364851515
+            ],
+            [
+              14.26094055175781,
+              67.21068245713607
+            ],
+            [
+              14.162063598632812,
+              67.18912942176102
+            ],
+            [
+              14.08172607421875,
+              67.17554909438454
+            ],
+            [
+              14.047393798828125,
+              67.16862284538965
+            ],
+            [
+              14.02679443359375,
+              67.14703216100033
+            ],
+            [
+              14.02130126953125,
+              67.12942546602565
+            ],
+            [
+              13.980789184570312,
+              67.10886809801592
+            ],
+            [
+              13.923797607421873,
+              67.08695661239739
+            ],
+            [
+              13.901138305664062,
+              67.07626088329239
+            ],
+            [
+              13.891525268554686,
+              67.0599408019667
+            ],
+            [
+              13.854446411132812,
+              67.04012791618119
+            ],
+            [
+              13.82080078125,
+              67.04548434579513
+            ],
+            [
+              13.776168823242188,
+              67.05324906915747
+            ],
+            [
+              13.46649169921875,
+              66.96178929585454
+            ],
+            [
+              13.46649169921875,
+              66.93974468064502
+            ],
+            [
+              13.511810302734375,
+              66.90690968501593
+            ],
+            [
+              13.54339599609375,
+              66.8772664784669
+            ],
+            [
+              13.590087890625,
+              66.84704735751328
+            ],
+            [
+              13.5516357421875,
+              66.82976253611592
+            ],
+            [
+              13.580474853515625,
+              66.82219659327606
+            ],
+            [
+              13.9801025390625,
+              66.80868017479824
+            ],
+            [
+              13.574981689453125,
+              66.81625028637526
+            ],
+            [
+              13.4088134765625,
+              66.80381244134801
+            ],
+            [
+              13.286590576171875,
+              66.77945929207732
+            ],
+            [
+              13.206939697265625,
+              66.74911942059309
+            ],
+            [
+              13.260498046875,
+              66.74152860435939
+            ],
+            [
+              13.46099853515625,
+              66.73718994427429
+            ],
+            [
+              13.53240966796875,
+              66.73122303857558
+            ],
+            [
+              13.55712890625,
+              66.71439943596735
+            ],
+            [
+              13.56536865234375,
+              66.70299627645545
+            ],
+            [
+              13.704071044921875,
+              66.70353940360214
+            ],
+            [
+              13.574981689453125,
+              66.71005599682081
+            ],
+            [
+              13.517303466796873,
+              66.73176554427145
+            ],
+            [
+              13.19732666015625,
+              66.74044401099103
+            ],
+            [
+              13.179473876953125,
+              66.7507457196216
+            ],
+            [
+              13.2110595703125,
+              66.77241944244105
+            ],
+            [
+              13.414306640625,
+              67.14223160765509
+            ],
+            [
+              14.363422393798826,
+              67.28358000241472
+            ]
+          ]
+        }
+      }
+    ]
+  }];
+// ---------------------
+// Oppsett av kartet
+document.body.onload = function() {
+    // Henter id til kartelementet i HTML
+    var mapId = $(".map")[0].id;
+    var mapData = null;
+    var mapView = null;
+
+    // Henter riktig data og setter kartets senter utifra kart ID
+    if (mapId === "map-lofoten") {
+        mapData = lofotenMapData;
+        mapView = [68.175040, 14.084761];
+
+    } else if (mapId === "map-henningsvaer") {
+        mapData = henningsvaerMapData;
+        mapView = [67.880712, 13.499450];
+
+    } else if (mapId === "map-bodoe") {
+        mapData = bodoeMapData;
+        mapView = [66.991330, 13.927917];  
+    }
+
+    var map = L.map(mapId, {
+        scrollWheelZoom: false
+    }).setView(mapView, 8);
+
+    // Ved klikk på kartet slår man av og på zooming ved scrolling
+    // i kartet. Scroll-funksjonen er skrudd av til å begynne med.
     map.on('click', function() {
         if (map.scrollWheelZoom.enabled()) {
           map.scrollWheelZoom.disable();
@@ -264,11 +702,13 @@ document.body.onload = function() {
           }
         });
 
+    // Henter bakgrunnskart fra Kartverket
     L.tileLayer('https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart&zoom={z}&x={x}&y={y}', {
         attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
         }).addTo(map);
     
-    L.geoJSON(lofotenMapData, {
+    // Legger inn dataene om reiseruten, og definerer popup på der hvor dette er definert som åpnes med klikk.
+    L.geoJSON(mapData, {
         onEachFeature: function onEachFeature(feature, layer) {
             if(feature.properties && feature.properties.popupContent) {
                 layer.bindPopup(feature.properties.popupContent);
