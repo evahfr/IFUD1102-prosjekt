219ee9c25567d5428283d4e067152a08a039168d
evahfr <evahfr@gmail.com>
2019-05-01 20:42:26 +0200

    Rettet feilmelding ved manglende URL parametere

--- Prosjekt/script/queryStringReader.js
+++ Prosjekt/script/queryStringReader.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
+
+    // Henter URL paramenterne og splitter dem opp i et array av strenger
     var queryString = window.location.search.substring(1);
     var queries = queryString.split("&");
     var cruiseDest = "";
     var cruiseId = "";
 
-    console.log(queries);
+    // Går igjennom alle URL parametrene
     queries.forEach( function(element) {
+
+        // Hvis det ikke er noen URL parametre returner
+        if (element === "") {
+            return;
+        }
+
         var paramName = element.split("=")[0].trim();
         var paramValue = element.split("=")[1].trim();
 
+        // Sjekker etter ønsket parameter; "cruiseId"
         if (paramName === "cruiseId") {
             cruiseId = paramValue;
 
